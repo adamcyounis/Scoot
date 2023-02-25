caa69a44e464a188b6eba871bc6a0e085f203851
Adam Younis <adam.younis@gmail.com>
2023-02-26 01:12:22 +0700

    Adds Team Switcher for VS mode

--- Assets/BigWordsManager.cs
+++ Assets/BigWordsManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class BigWordsManager : MonoBehaviour {
+    public AudioClip a_ready;
+    public AudioClip a_go;
+    public AudioClip a_gameSet;
 
     public Sprite ready;
     public Sprite go;
@@ -23,12 +26,15 @@ public class BigWordsManager : MonoBehaviour {
         Time.timeScale = 0;
         image.enabled = true;
         image.sprite = ready;
+        SoundSystem.system.PlaySFX(a_ready);
         StartCoroutine(PrepareToGo());
     }
 
     IEnumerator PrepareToGo() {
-        yield return new WaitForSecondsRealtime(1);
+        yield return new WaitForSecondsRealtime(2);
         image.sprite = go;
+        SoundSystem.system.PlaySFX(a_go);
+
         Time.timeScale = 1;
         yield return new WaitForSecondsRealtime(1);
         image.sprite = null;
@@ -39,6 +45,8 @@ public class BigWordsManager : MonoBehaviour {
     public void EndMatch() {
         image.enabled = true;
         image.sprite = gameSet;
+        SoundSystem.system.PlaySFX(a_gameSet);
+
         Time.timeScale = 0.1f;
     }
 }
---
+++ Assets/TeamSwitcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamSwitcher : MonoBehaviour {
+    public int team;
+    public BoxCollider2D switcher;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.root.GetComponentInChildren<Life>() is Life l) {
+            l.team = team;
+        }
+    }
+}
